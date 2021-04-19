14c0228b4480feefff15485ac9264507c8f290f3
Young Jung <YoungJung365@gmail.com>
2021-04-19 12:47:39 -0700

    Use Object for payments detail and summary

--- js/mortgage.js
+++ js/mortgage.js
@@ -1,26 +1,40 @@
- var curPayments = [];
+ var paymentDS = {
+     paymentsArray: [],
+     summary: {}
+ };
 
  // When the user clicks the button
  function buildPaymentSchedule() {
      // Validation first
      let amt = parseFloat(document.getElementById("newLoanAmt").value);
-     let term = parseFloat(document.getElementById("newPayPeriod").value);
+     let year = parseInt(document.getElementById("newPayPeriod").value);
      let rate = parseFloat(document.getElementById("newRate").value);
-     if (isNaN(amt) || isNaN(term) || isNaN(rate)) {
+
+     if (isValidInput(amt, year, rate)) {
+         paymentDS = getPayments(amt, (year * 12), rate);
+         displayPart(paymentDS);
+     }
+ }
+
+ function displayPart(paymentDS) {
+     displayData(paymentDS.paymentsArray);
+     displaySummary(paymentDS.summary);
+ }
+
+ function isValidInput(amt, year, rate) {
+
+     if (isNaN(amt) || isNaN(year) || isNaN(rate)) {
          //alert("number needed!!!");
          swal(
              'Oops!',
              'You need to input <b style="color:red;">number</b> only!',
              'error'
          );
-     } else {
-         // Get some values
-         paymentsArray = getPayments(amt, term, rate);
-         displayData(paymentsArray);
-         displaySummary(paymentsArray);
+         return false;
      }
- }
 
+     return true;
+ }
 
  function getPayments(amtLoaned, term, rate) {
      let monthPayments = (amtLoaned) * (rate / 1200) / (1 - (Math.pow((1 + rate / 1200), (-term))));
@@ -44,43 +58,20 @@
          obj["totalInterest"] = totalInterest;
          remainBalance = remainBalance - principalPay;
          obj["balance"] = remainBalance;
-         curPayments.push(obj);
+         paymentDS.paymentsArray.push(obj);
      }
-
-     return curPayments;
+     paymentDS.summary.monthPayments = monthPayments;
+     paymentDS.summary.totalPayments = amtLoaned;
+     paymentDS.summary.totalInterest = totalInterest;
+     paymentDS.summary.totalCost = amtLoaned + totalInterest;
+     return paymentDS;
  }
 
- function displaySummary(curPayments) {
-     let dispTotalPayments = curPayments[0].balance + curPayments[0].principal;
-     let disPay = curPayments[0].payment;
-     let disTotalInt = curPayments[curPayments.length - 1].totalInterest;
-     document.getElementById("monthlyPayment").innerHTML = disPay.toLocaleString('en-US', {
-         style: 'currency',
-         currency: 'USD',
-     });
-     document.getElementById("totalPrincipal").innerHTML = dispTotalPayments.toLocaleString('en-US', {
-         style: 'currency',
-         currency: 'USD',
-     })
-     document.getElementById("totalInterest").innerHTML = disTotalInt.toLocaleString('en-US', {
-         style: 'currency',
-         currency: 'USD',
-     })
-     document.getElementById("totalCost").innerHTML = (dispTotalPayments + disTotalInt).toLocaleString('en-US', {
-         style: 'currency',
-         currency: 'USD',
-     })
- }
-
- function displayData(paymentsArray) {
+ function displayData(curPayments) {
      const template = document.getElementById("Mortgage-Data-Template");
      const resultsBody = document.getElementById("mortgateResults");
      // Clear table first
      resultsBody.innerHTML = "";
-
-     //  if (curPayments.length == 0) {
-     //      curPayments = paymentsArray;
-     //  }
 
      for (let i = 0; i < curPayments.length; i++) {
          const dataRow = document.importNode(template.content, true);
@@ -89,9 +80,24 @@
          dataRow.getElementById("principal").textContent = (curPayments[i].principal).toFixed(2);
          dataRow.getElementById("interest").textContent = (curPayments[i].interest).toFixed(2);
          dataRow.getElementById("totalInterest").textContent = (curPayments[i].totalInterest).toFixed(2);
-         dataRow.getElementById("balance").textContent = (curPayments[i].balance).toFixed(2);
+         dataRow.getElementById("balance").textContent = Math.abs((curPayments[i].balance)).toFixed(2);
 
          // Add the row to the page
          resultsBody.appendChild(dataRow);
      }
+ }
+
+ function displaySummary(summary) {
+
+     document.getElementById("monthlyPayment").innerHTML = displayTwoDigit(summary.monthPayments);
+     document.getElementById("totalPrincipal").innerHTML = displayTwoDigit(summary.totalPayments);
+     document.getElementById("totalInterest").innerHTML = displayTwoDigit(summary.totalInterest);
+     document.getElementById("totalCost").innerHTML = displayTwoDigit(summary.totalCost);
+ }
+
+ function displayTwoDigit(amt) {
+     return amt.toLocaleString('en-US', {
+         style: 'currency',
+         currency: 'USD',
+     });
  }
